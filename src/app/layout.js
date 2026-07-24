25e9526a47f1949009c319b6be80d842ad4bfbb2
pyoska <pyoska1@gmail.com>
2026-07-24 14:14:25 +0900

    fix: update Daum Webmaster Tool verification token

--- src/app/layout.js
+++ src/app/layout.js
@@ -52,7 +52,7 @@ export default function RootLayout({ children }) {
           strategy="afterInteractive"
         />
         {/* Daum Webmaster Tool Verification Comment */}
-        <div dangerouslySetInnerHTML={{ __html: '<!-- DaumWebMasterTool:31e1c1e9a77ec5eaba225de70a3afe1eb1ed94099e0576c16c85bb140568ac89:NSllVgtQeByhwmkxZnNyHA== -->' }} />
+        <div dangerouslySetInnerHTML={{ __html: '<!-- DaumWebMasterTool:31e1c1e9a77ec5eaba225de70a3afe1eb1ed94099e0576c16c85bb140568ac89:JbtVqj5ycYCrCKnoguOyqw== -->' }} />
         {children}
       </body>
     </html>
